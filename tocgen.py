4b0c0060169bbdc07a6e41d4bf4781e50a948c92
Alex Beal <alexlbeal@gmail.com>
2012-09-11 16:51:30 -0600

    Adds HTML header and footer. Table of contents generated!
    - Forgot to add the HTML that the HTMLGen stage uses. Oops!
    - Now generates a simple table of contents.

--- tocgen.py
+++ tocgen.py
@@ -1,3 +1,5 @@
+import datetime
+
 class TocGen(object):
     single_item=False
 
@@ -5,4 +7,28 @@ class TocGen(object):
         pass
 
     def process(self, post_list):
+        post_links = ""
+        for post in post_list:
+            post_links += "<p>" + self._gen_link(post) + "\n"
+        with open("body_head_toc") as header_file:
+            header_str = header_file.read()
+            header_str = header_str
+            body = header_str + post_links
+        with open("body_footer") as footer_file:
+            body += footer_file.read()
+        post_list.append({
+            'title':"/usr/sbin/blog",
+            'author':"Alex Beal",
+            'date': datetime.datetime.now(),
+            'permalink': "index.html",
+            'body': post_links,
+            'cur_res': body
+            })
+
         return post_list
+
+    def _gen_link(self, post):
+        return '<a href="%s">%s</a> - %s' % (
+                post["permalink"],
+                post["title"],
+                post["date"].strftime("%m/%d/%Y"))
